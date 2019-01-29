fda81c99f72fab06456ecff75abe52b6360d1f99
vikrantpch <vikrant.p.ch@gmail.com>
2019-01-28 23:47:12 -0500

    create and setup bucket

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,5 +1,9 @@
 import boto3
 import click
+from botocore.exceptions import ClientError
+
+session = boto3.Session(profile_name='pythonAutomation')
+s3=session.resource('s3')
 
 @click.group()
 def cli():
@@ -21,8 +25,50 @@ def list_bucket_objects(bucket):
         print(obj)
     return
 
-session = boto3.Session(profile_name='pythonAutomation')
-s3=session.resource('s3')
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    "Create and Configure S3 Bucket"
+    s3_bucket = None
+    try:
+        s3_bucket = s3.create_bucket(
+        Bucket=bucket,
+        CreateBucketConfiguration={'LocationConstraint': session.region_name}
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            s3_bucket = s3.Bucket(bucket)
+        else:
+            raise e
+    policy="""
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "PublicReadGetObject",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": "s3:GetObject",
+                "Resource": "arn:aws:s3:::%s/*"
+            }
+        ]
+    }
+    """%s3_bucket.name
+    policy=policy.strip()
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+    ws=s3_bucket.Website()
+    ws.put( WebsiteConfiguration={
+        'ErrorDocument': {
+            'Key': 'error.html'
+        },
+        'IndexDocument': {
+            'Suffix': 'index.html'
+        }
+        }
+        )
+    return
+
 
 if __name__ == '__main__':
     cli()

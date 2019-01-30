92db21374604ea175c35be194af5f473b457b1e2
vikrantpch <vikrant.p.ch@gmail.com>
2019-01-30 07:17:36 -0500

    clean up code style per pycodestyle, pydocstyle, and pylint.

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,48 +1,66 @@
-import boto3
-import click
-from botocore.exceptions import ClientError
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Webotron: Deploy websites with AWS.
+
+Webotron automates the process of deploying static websites to AWS.
+- Configure AWS S3 buckets
+ - Create them
+ - Set them up for static website hosting
+ - Deploy local files to them
+- Configure DNS with AWS Route 53
+- Configure a Content Delivery Network and SSL with AWS CloudFront
+"""
+
 from pathlib import Path
 import mimetypes
 
+import boto3
+from botocore.exceptions import ClientError
+import click
+
+
 session = boto3.Session(profile_name='pythonAutomation')
-s3=session.resource('s3')
+s3 = session.resource('s3')
+
 
 @click.group()
 def cli():
-    "Webotron deploys websites to AWS"
+    """Webotron deploys websites to AWS."""
     pass
+
 
 @cli.command('list-buckets')
 def list_buckets():
-    "List All S3 Buckets"
+    """List All S3 Buckets."""
     for bucket in s3.buckets.all():
         print(bucket.name)
     return
 
+
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-    "List Objects in S3 Bucket"
+    """List Objects in S3 Bucket."""
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
     return
 
+
 @cli.command('setup-bucket')
 @click.argument('bucket')
 def setup_bucket(bucket):
-    "Create and Configure S3 Bucket"
+    """Create and Configure S3 Bucket."""
     s3_bucket = None
     try:
-        s3_bucket = s3.create_bucket(
-        Bucket=bucket,
-        CreateBucketConfiguration={'LocationConstraint': session.region_name}
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+        s3_bucket = s3.create_bucket(Bucket=bucket, CreateBucketConfiguration={
+            'LocationConstraint': session.region_name})
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
             s3_bucket = s3.Bucket(bucket)
         else:
-            raise e
-    policy="""
+            raise error
+    policy = """
     {
         "Version": "2012-10-17",
         "Statement": [
@@ -55,43 +73,46 @@ def setup_bucket(bucket):
             }
         ]
     }
-    """%s3_bucket.name
-    policy=policy.strip()
+    """ % s3_bucket.name
+    policy = policy.strip()
     pol = s3_bucket.Policy()
     pol.put(Policy=policy)
-    ws=s3_bucket.Website()
-    ws.put( WebsiteConfiguration={
+    s3_bucket.Website().put(WebsiteConfiguration={
         'ErrorDocument': {
             'Key': 'error.html'
         },
         'IndexDocument': {
             'Suffix': 'index.html'
         }
-        }
-        )
+        })
     return
 
+
 def upload_file(s3_bucket, path, key):
+    """Upload path to s3_bucket at key."""
     content_type = mimetypes.guess_type(key)[0] or 'text/plain'
     s3_bucket.upload_file(
         path,
         key,
         ExtraArgs={
-            'ContentType':content_type
+            'ContentType': content_type
         })
 
 
 @cli.command('sync')
-@click.argument('pathname',type=click.Path(exists=True))
+@click.argument('pathname', type=click.Path(exists=True))
 @click.argument('bucket')
-def sync(pathname,bucket):
-    "SYNC contents of PATHNAME to BUCKET"
+def sync(pathname, bucket):
+    """SYNC contents of PATHNAME to BUCKET."""
     s3_bucket = s3.Bucket(bucket)
     root = Path(pathname).expanduser().resolve()
+
     def handle_directory(target):
         for p in target.iterdir():
-            if p.is_dir():handle_directory(p)
-            if p.is_file():upload_file(s3_bucket,str(p),str(p.relative_to(root)))
+            if p.is_dir():
+                handle_directory(p)
+            if p.is_file():
+                upload_file(s3_bucket, str(p), str(p.relative_to(root)))
 
     handle_directory(root)
 
